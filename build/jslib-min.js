5f2e301d6df8f0a27bf94dad3bf62feb08f6699e
André BERLEMONT <andre.berlemont@gmail.com>
2016-03-03 14:25:17 +0100

    build

--- build/jslib-min.js
+++ build/jslib-min.js
@@ -170,25 +170,68 @@ Display.onResize = function(callback, intervalTime){
 
 }
 
-Input = function(){}
+/*
+  32 = space
+  37 = left
+  38 = up
+  39 = right
+  40 = down
+*/
+var INPUT;
 
+$(function(){
+  INPUT = new Input();
+})
 
-Input.assignKey = function(charCode, callback){
+Input = function(){
+  this.onPressCallback = [];
 
-  //http://www.javascriptkit.com/jsref/eventkeyboardmouse.shtml
-  document.onkeypress=function(e){
-    //console.log(e);console.log(charCode);
-    var e=window.event || e;
-    if(e.charCode == charCode){
-      if(callback != undefined) callback();
-    }
-
-    //alert("CharCode value: "+e.charCode);
-    //alert("Character: "+String.fromCharCode(e.charCode));
+  this.setup = function(){
+    document.onkeydown = this.onKeyPress;
   }
 
+  this.onKeyPress = function(evt){
+    var code = Input.extractCharCode(evt);
+    
+    console.log(code+" / "+INPUT.onPressCallback.length);
+
+    if(code < INPUT.onPressCallback.length){
+      if(INPUT.onPressCallback[code] != undefined){
+        console.log(code);
+        INPUT.onPressCallback[code]();
+      }  
+    }
+    
+  }
+
+  this.setup();
 }
 
+Input.extractCharCode = function(evt){
+
+  if (!evt) evt = window.evt;
+  var code = evt.keyCode;
+  if (evt.charCode && code == 0) code = evt.charCode;
+
+  return code;
+}
+
+Input.alertOnKey = function(){
+  document.onkeydown = function(evt){
+    var code = Input.extractCharCode(evt);
+    alert(keyCode);
+  }
+}
+
+Input.assignKey = function(charCode, callback, owner){
+  INPUT.onPressCallback[charCode] = callback;
+  if(owner != undefined){
+    console.log(charCode+" assigned for "+owner);
+  }
+  
+}
+
+/* mouse attachement to DOM element */
 Input.click = function(elmt, callback){
   elmt.bind("click touchstart", callback);
 }
